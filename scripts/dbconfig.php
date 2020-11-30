1adfd073489c58cd9608be5229aa0a6fd1b2a99e
Dahalia Howell <dahaliahowell@Dahalias-MacBook-Pro.local>
2020-11-30 09:06:08 -0500

    Created scripts for database handling

---
+++ scripts/dbconfig.php
@@ -0,0 +1,5 @@
+<?php header('Access-Control-Allow-Origin: *'); 
+    $host = 'localhost';
+    $dbname = 'latched_ja_database';
+    $username = 'comp2140_student';
+    $password = 'PmEGW3L7fkKXRzvA';
