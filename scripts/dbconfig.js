2a3a7744c4e886d2039987373b1fe133e7897aca
Dahalia Howell <dahaliahowell@Dahalias-MacBook-Pro.local>
2020-11-30 23:18:19 -0500

    Added AJAX configurations

--- scripts/dbconfig.js
+++ scripts/dbconfig.js
@@ -1,7 +1,9 @@
 window.onload = function() {
 
-    var itemSubmit = document.getElementById("");
-    var categorySubmit = document.getElementById("");
+    const itemSubmit = document.getElementById("");
+    const categorySubmit = document.getElementById("");
+    const userAdd = document.getElementById("");
+    const updatebtn = document.getElementById("");
     var httpRequest;
   
     itemSubmit.addEventListener('click', function(element) {
@@ -17,24 +19,59 @@ window.onload = function() {
       httpRequest.send('itemName=' + encodeURIComponent(itemName));
     });
 
-    searchBtnCities.addEventListener('click', function(element) {
+    categorySubmit.addEventListener('click', function(element) {
         element.preventDefault();
     
         httpRequest = new XMLHttpRequest();
-        var category = document.querySelector('').value;
+        const category = document.querySelector('').value;
     
         var url = "http://localhost:8888/Latched-Ja.-Inventory-Management-System/scripts/categories.php?category=" + category;
         httpRequest.onreadystatechange = sendResult;
         httpRequest.open('GET', url);
         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         httpRequest.send('category=' + encodeURIComponent(category));
-      });
+    });
+
+    updatebtn.addEventListener('click', function(element) {
+        element.preventDefault();
+    
+        httpRequest = new XMLHttpRequest();
+        const itemId = document.querySelector('').value;
+        const qty = document.querySelector('').value;
+        const status = document.querySelector('').value;
+    
+        var url = "http://localhost:8888/Latched-Ja.-Inventory-Management-System/scripts/categories.php?itemid=" + itemId + "&context=" + status.toLowerCase();
+        httpRequest.onreadystatechange = sendResult;
+        httpRequest.open('GET', url);
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        httpRequest.send('id=' + encodeURIComponent(itemId) + '&qty=' + encodeURIComponent(qty) + '&status=' + encodeURIComponent(status.toLowerCase()));
+    });
+
+    userAdd.addEventListener('click', function(element) {
+        element.preventDefault();
+    
+        httpRequest = new XMLHttpRequest();
+        const name = document.querySelector('').value;
+        const username = document.querySelector('').value;
+        const role = document.querySelector('').value;
+        const password = document.querySelector('').value;
+        const email = document.querySelector('').value;
+        const contact_num = document.querySelector('').value;
+    
+        var url = "http://localhost:8888/Latched-Ja.-Inventory-Management-System/scripts/user.php?context=newuser";
+        httpRequest.onreadystatechange = sendResult;
+        httpRequest.open('POST', url);
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        httpRequest.send('name=' + encodeURIComponent(name) + '&username=' + encodeURIComponent(username) + '&role=' +
+                        encodeURIComponent(role) + '&password=' + encodeURIComponent(password) + '&email=' + 
+                        encodeURIComponent(email) + '&contactnum=' + encodeURIComponent(contact_num));
+    });
   
     function sendResult() {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
           var response = httpRequest.responseText;
-          document.getElementById("result").innerHTML = response;
+          document.getElementById("").innerHTML = response;
         } else {
           alert('There was a problem with the request.');
         }
