2fecd089e94fa5b61973f189bdec39f45ad5a94b
Dahalia Howell <dahaliahowell@Dahalias-MacBook-Pro.local>
2020-12-02 23:31:16 -0500

    Added more configuations to js and php

--- scripts/dbconfig.js
+++ scripts/dbconfig.js
@@ -3,10 +3,11 @@ window.onload = function() {
     const itemSubmit = document.getElementById("");
     const categorySubmit = document.getElementById("");
     const userAdd = document.getElementById("");
-    const updatebtn = document.getElementById("");
+    // const updatebtn = document.getElementById("updateLink");
+    const updatebtn = document.getElementById("searchinventorybtn");
     var httpRequest;
   
-    itemSubmit.addEventListener('click', function(element) {
+    /*itemSubmit.addEventListener('click', function(element) {
       element.preventDefault();
   
       httpRequest = new XMLHttpRequest();
@@ -30,24 +31,28 @@ window.onload = function() {
         httpRequest.open('GET', url);
         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         httpRequest.send('category=' + encodeURIComponent(category));
-    });
+    });*/
 
     updatebtn.addEventListener('click', function(element) {
         element.preventDefault();
+
+        console.log('button clicked')
     
         httpRequest = new XMLHttpRequest();
-        const itemId = document.querySelector('').value;
-        const qty = document.querySelector('').value;
-        const status = document.querySelector('').value;
+        const itemId = document.querySelector('#searchinput').value;
+        // const qty = document.querySelector('').value;
+        // const status = document.querySelector('').value;
     
-        var url = "http://localhost:8888/Latched-Ja.-Inventory-Management-System/scripts/categories.php?itemid=" + itemId + "&context=" + status.toLowerCase();
+        // var url = "http://localhost:8888/Latched-Ja.-Inventory-Management-System/scripts/items.php?itemid=" + itemId + "&context=" + status.toLowerCase();
+        var url = "http://localhost:8888/Latched-Ja.-Inventory-Management-System/scripts/items.php?itemid=" + itemId;
         httpRequest.onreadystatechange = sendResult;
         httpRequest.open('GET', url);
         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        httpRequest.send('id=' + encodeURIComponent(itemId) + '&qty=' + encodeURIComponent(qty) + '&status=' + encodeURIComponent(status.toLowerCase()));
+        // httpRequest.send('id=' + encodeURIComponent(itemId) + '&qty=' + encodeURIComponent(qty) + '&status=' + encodeURIComponent(status.toLowerCase()));
+        httpRequest.send('id=' + encodeURIComponent(itemId));
     });
 
-    userAdd.addEventListener('click', function(element) {
+    /*userAdd.addEventListener('click', function(element) {
         element.preventDefault();
     
         httpRequest = new XMLHttpRequest();
@@ -65,13 +70,13 @@ window.onload = function() {
         httpRequest.send('name=' + encodeURIComponent(name) + '&username=' + encodeURIComponent(username) + '&role=' +
                         encodeURIComponent(role) + '&password=' + encodeURIComponent(password) + '&email=' + 
                         encodeURIComponent(email) + '&contactnum=' + encodeURIComponent(contact_num));
-    });
+    });*/
   
     function sendResult() {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
           var response = httpRequest.responseText;
-          document.getElementById("").innerHTML = response;
+          document.getElementById("inventorytable").innerHTML = response;
         } else {
           alert('There was a problem with the request.');
         }
--- scripts/updateInventory.php
+++ scripts/updateInventory.php
@@ -26,6 +26,9 @@ try {
             echo "CANNOT PROCESS REQUEST";
 
         }
+        echo $id;
+        $stmt = $conn->query("SELECT * FROM items WHERE id LIKE '%$id%'");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
       
     }
 
@@ -35,3 +38,34 @@ try {
   
   $conn = null;
 ?>
+
+<table class="table table-light table-bordered table-hover table-responsive-xl">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col" id="itemidhead">Item ID</th>
+            <th scope="col" id="itemnamehead">Item Name</th>
+            <th scope="col" id="itemtypehead">Item Type</th>
+            <!--<th scope="col" id="itemmaterialhead">Item Material</th>
+            <th scope="col" id="itemcolourhead">Item Colour</th>-->
+            <th scope="col" id="itemquantityhead">Item Current Quantity</th>
+            <th scope="col" id="itempricehead">Item Sell Price</th>
+            <th scope="col" id="itempricehead">Item Purchase Price</th>
+            <th scope="col" id="itemsupplierhead">Supplier</th>
+        </tr>    
+    </thead>
+    <tbody>
+    <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= $row['id']; ?></td>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['type']; ?></td>
+            <td><?= $row['qty']; ?></td>
+            <td><?= '$' . $row['sell_price']; ?></td>
+            <td><?= '$' . $row['purchase_price']; ?></td>
+            <td><?= $row['supplier']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+    <tfoot>
+    </tfoot>
+</table> 
